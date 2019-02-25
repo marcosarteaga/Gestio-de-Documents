235755fb612103e974532f006c533e6ae1ca8fbd
marcosarteaga <marcos118118@gmail.com>
2019-02-25 20:25:10 +0100

    formulario

--- GDD/resources/views/componentes/Clientes.blade.php
+++ GDD/resources/views/componentes/Clientes.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <h1>Clientes</h1>
+<img style="left:right;" height="5%" width="5%" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzauaghT7_ny88d8mB8PhVBeKmR7OHxpRynskzQNuvnY5ymueAvQ">
 <ul class="list-group">
    @foreach( $arrayClientes as $key => $Cliente )
     <li class="list-group-item">
