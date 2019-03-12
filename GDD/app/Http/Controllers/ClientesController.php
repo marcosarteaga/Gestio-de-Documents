ce20e078127b7920588b44f90ac5c85ff7d2808f
super <usuari@domini.com>
2019-03-12 18:01:23 +0100

    Insert + post

--- GDD/app/Http/Controllers/ClientesController.php
+++ GDD/app/Http/Controllers/ClientesController.php
@@ -5,36 +5,61 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Cliente;
 use App\ventas;
-
+use Exception;
 
 
 class ClientesController extends Controller
 {
+    //mostrar clientes
     public function getClientes()
     {
         $arrayClientes = Cliente::all();
         return view('componentes.Clientes',array('arrayClientes'=> $arrayClientes));
     }
-    public function getCreateClient()
-    {
-        return view('componentes.formClientes');
-    }
+    //mostrar cliente
     public function showClient($id)
     {
         $cliente = Cliente::find($id);
         return view('detalle.cliente',array('cliente'=>$cliente));
     }
-/* preguntar a enric
-    public function showVentas($id)
+    //vista formulario
+    public function getCreateClient()
     {
-        $venta = ventas::find($id);
-        return view('detalle.cliente',array('venta'=>$venta));
+        return view('componentes.formClientes');
     }
-*/
+    //añadir cliente
+    public function saveClient(Request $request){
+		try{
+			$cliente = new Cliente;
+                $cliente->Nom = $request->input('Nom');
+                $cliente->Cognom = $request->input('Cognom');
+				$cliente->Email = $request->input('Email');
+				$cliente->Telefon = $request->input('Telefon');
+				$cliente->Direccion = $request->input('Direccion');
+				$cliente->NIF = $request->input('NIF');
+				$cliente->Provincia = $request->input('Provincia');
+				$cliente->Localidad = $request->input('Localidad');
+				$cliente->CP = $request->input('CP');
+            $cliente->save();
+            $arrayClientes = Cliente::all();
+			//$clientes = Cliente::select('id', 'Nom', 'Cognom','Email', 'Telefon', 'Direccio', 'NIF', 'Provincia','Localidad','CP')->get();
+			return view('componentes.Clientes', ['arrayClientes'=>$arrayClientes]);
+		}catch(Exception $e){
+			return back()->withErrors(['SVError'=>'Error del servidor @Save']);		
+		}
+	}
     public function insertClient(Request $request)
     {
         Cliente::create($request->all());
         $arrayClientes = Cliente::all();
         return view('componentes.Clientes',array('arrayClientes'=> $arrayClientes));
     }
+
+    /* preguntar a enric
+    public function showVentas($id)
+    {
+        $venta = ventas::find($id);
+        return view('detalle.cliente',array('venta'=>$venta));
+    }
+*/
 }
